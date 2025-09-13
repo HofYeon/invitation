98777d428dc3327cb2b9e4c9202f74fd77a7ab09
Minhong <wjddn251301@gmail.com>
2025-09-13 21:26:39 +0900

    init : docker 컨테이너 설정과 invitation 모델 생성

---
+++ backend/core/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+# class Invitation(models.Model):
+#     user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     groom_lastname = models.char
